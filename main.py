d28e3a0587f1204bcd658cf5986501487a862528
Jack Huang <719668276@qq.com>
2022-11-14 08:17:57 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+#!/usr/bin/env python
 import pyautogui, time
 
-def extendSession():
+def extendSession(x, y):
     currentMouseX, currentMouseY = pyautogui.position()
-    print(currentMouseX, currentMouseY)
-    iconX, iconY = 778, 10
-    pyautogui.click(iconX, iconY)
+    pyautogui.click(x, y)
     pyautogui.move(0, 100)
     time.sleep(0.2)
     pyautogui.move(220, 0)
@@ -12,8 +11,19 @@ def extendSession():
     pyautogui.move(0, 110)
     pyautogui.click()
     pyautogui.moveTo(currentMouseX, currentMouseY)
+    print('Extend session triggerd')
 
-while True:
-    extendSession()
-    #time.sleep(10*60)
-    time.sleep(10)
+
+def main():
+    print('Put the mouse curser to the icon first:')
+    countNum = 5
+    for i in range(countNum):
+        print('Counting down {} s ...'.format(countNum-i))
+        time.sleep(1)
+    iconX, iconY = pyautogui.position()
+    while True:
+        extendSession(iconX, iconY)
+        time.sleep(10*60)
+
+if __name__ == '__main__':
+    main()
